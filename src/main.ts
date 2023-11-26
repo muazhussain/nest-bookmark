2817848ca29bab023431166b279843f25e5ece7a
muazhussain <muaz.self@gmail.com>
2023-11-26 07:23:01 +0600

    Add
    Swagger add.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as basicAuth from 'express-basic-auth';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  app.use(
+    ['/docs', '/docs-json'],
+    basicAuth({
+      challenge: true,
+      users: {
+        ['muaz']: '12345',
+      },
+    }),
+  );
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest Bookmark API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+      caches: 'no-cache',
+    },
+  });
+
+  await app.listen(3011);
 }
 bootstrap();
