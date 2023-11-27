d14322b1dd724c14cbf57d2de7e15d1e1bb100d5
muazhussain <muaz.self@gmail.com>
2023-11-27 08:46:08 +0600

    Create
    Prisma module create to access DB.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, BookmarksModule],
+  imports: [AuthModule, BookmarksModule, PrismaModule],
 })
 export class AppModule {}
