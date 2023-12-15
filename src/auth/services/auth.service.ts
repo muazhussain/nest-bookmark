725a7edb9ede75c5b12c209cd19ce0d2585c8549
muazhussain <muaz.self@gmail.com>
2023-12-15 15:32:08 +0600

    Update

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from '../dtos/sign-up.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly prisma: PrismaService) { }
+    async signUp(payload: AuthDto) {
+        // create user
+        const user = await this.prisma.user
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as basicAuth from 'express-basic-auth';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+    whitelist: true,
+  }));
+
+  app.enableCors({ exposedHeaders: ['Content-Disposition'] })
+
+  // swagger
   app.use(
     ['/docs', '/docs-json'],
     basicAuth({
